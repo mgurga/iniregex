f64fbb0438f084c6bed303e3b69d9e9a010aa3b9
murga <motiurga@gmail.com>
2021-02-05 17:35:38 -0800

    finished refrence class with basic test

--- src/iniregex/inivalidator.java
+++ src/iniregex/inivalidator.java
@@ -1,6 +1,12 @@
 package iniregex;
 
+import java.util.HashMap;
+
 public class inivalidator {
+	
+	private static final String commentregex = ";.*";
+	private static final String sectionheaderregex = "\\[.*\\]";
+	private static final String propertyvalueregex = ".*=[ \\w.//]*";
 	
 	// checks based on rough ini standard
 	// [section]
@@ -8,20 +14,96 @@ public class inivalidator {
 	// ; comment
 	
 	private String[] filelines;
+	private HashMap<String, HashMap<String, String>> inihm = new HashMap<String, HashMap<String, String>>();
 	
 	public inivalidator(String[] file) {
 		this.filelines = file;
 	}
 	
 	public boolean isValid() {
-		
+		boolean out = true;
 		for(int i = 0; i < filelines.length; i++) {
-			
+			if(!isLineValid(filelines[i])) {
+				out = false;
+			}
 		}
-		
+		return out;
 	}
 	
-	public String getProperty() {
+	public void loadData() {
+		String curSection = "";
+		boolean readingSection = false;
+		HashMap<String, String> properties = new HashMap<String, String>();
 		
+		for(int i = 0; i < filelines.length; i++) {
+			String line = filelines[i];
+			if(!readingSection && line.matches(sectionheaderregex)) {
+				// section header detected, starting reading properties
+				readingSection = true;
+				curSection = line.replaceAll("\\[|\\]", "");
+				properties = new HashMap<String, String>();
+			}
+			
+			if(readingSection && line.matches(propertyvalueregex)) {
+				String[] propval = line.split("=");
+				properties.put(propval[0], propval[1]);
+			}
+			
+			if(readingSection && line.equals("")) {
+				// empty line is section reading terminator
+				readingSection = false;
+				inihm.put(curSection, properties);
+			}
+			
+			if(readingSection && line.matches(sectionheaderregex)) {
+				// start reading section directly after property
+				inihm.put(curSection, properties);
+				curSection = line.replaceAll("\\[|\\]", "");
+				properties = new HashMap<String, String>();
+			}
+		}
+		
+		if(readingSection) {
+			inihm.put(curSection, properties);
+		}
+	}
+	
+	private boolean isLineValid(String line) {
+		if(line.matches(sectionheaderregex)) {
+			// line is a section header
+			return true;
+		} else if(line.matches(propertyvalueregex)) {
+			// line is property=value 
+			return true;
+		} else if(line.matches(commentregex)) {
+			// line is a comment
+			return true;
+		} else if(line.equals("")) {
+			// line is empty
+			return true;
+		} else {
+			System.out.println("line '" + line + "' is not valid");
+			return false;
+		}
+	}
+	
+	public String[] listSections() {
+		String[] keyarr = new String[inihm.keySet().size()];
+		for(int i = 0; i < keyarr.length; i++) {
+			keyarr[i] = inihm.keySet().toArray()[i].toString();
+		}
+		return keyarr;
+	}
+	
+	public String[] listSectionKeys(String section) {
+		String[] keyarr = new String[inihm.get(section).keySet().size()];
+		for(int i = 0; i < keyarr.length; i++) {
+			keyarr[i] = inihm.get(section).keySet().toArray()[i].toString();
+		}
+		return keyarr;
+	}
+	
+	public String getSectionKey(String section, String key) {
+		return inihm.get(section).get(key);
 	}
 }
--- src/iniregex/inivalidatordriver.java
+++ src/iniregex/inivalidatordriver.java
@@ -2,6 +2,37 @@ package iniregex;
 
 public class inivalidatordriver {
 	public static void main(String[] args) {
+		String[] correctini = new String[]{
+			"; TEST REGEX",
+			"",
+			"[demosection]",
+			"prop1=he llo/asd",
+			"prop2=worl d/qwe",
+			"foo=bar",
+			""
+		};
 		
+		inivalidator ir = new inivalidator(correctini);
+		
+		System.out.println(ir.isValid()); // validate
+		
+		ir.loadData(); // loads all sections and properties
+		
+		// print all sections
+		String[] sections = ir.listSections();
+		for(String i : sections) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		// print all keys in section
+		String[] keys = ir.listSectionKeys("demosection");
+		for(String i : keys) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		// get value from section key
+		System.out.println(ir.getSectionKey("demosection", "prop1"));
 	}
 }
